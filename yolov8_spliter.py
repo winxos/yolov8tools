a6d953dc664cdf19204aaddd66a42de86e452676
wvv <winxos@xhu.edu.cn>
2023-12-23 19:11:44 +0800

    spliter support cls

--- yolov8_spliter.py
+++ yolov8_spliter.py
@@ -27,10 +27,11 @@ with the setting ratio.
 '''
 import argparse
 import os
+from pathlib import Path,PurePath
 import random
 import shutil 
 from tqdm import tqdm
-
+IMG_FORMATS = 'bmp', 'jpeg', 'jpg'
 def move_data(path,dst,tags):
     img_dir = f"{path}/{dst}/images"
     label_dir = f"{path}/{dst}/labels"
@@ -47,35 +48,62 @@ def move_data(path,dst,tags):
         except:
             print(f"copy err. {t}")
     print("done.")
+def move_data_cls(path,dst,tags):
+    dst_dir = f"{path}/{dst}"
+    os.makedirs(dst_dir, exist_ok=True)
+    for img in tqdm(tags, desc=f"moving {dst}", ascii=" =", colour="green"):
+        try:
+            a_path = PurePath(img)
+            subd = str(a_path.relative_to(f"{path}/images")).split("\\")[0]
+            fn = "-".join(str(a_path.relative_to(f"{path}/images")).split("\\")[1:])
+            os.makedirs(f"{dst_dir}/{subd}", exist_ok=True)
+            dst = f"{dst_dir}/{subd}/{fn}"
+            shutil.copy(img,dst)
+        except:
+            print(f"copy err. {img}")
+    print("done.")  
 def main():
     parser = argparse.ArgumentParser(description="yolov8 converter")
     parser.add_argument("--proj",default="hand",help="project dir name")
     parser.add_argument("--proportion",default=0.8,type=float,help="train data proportion,default 0.8, [0.0 - 1.0]")
     parser.add_argument("--shuffle",default=0,type=int,help="random split the dataset,default 0, [0|1]")
+    parser.add_argument("--mode",default="seg",help="mode:seg,cls. cls no need labels")
     args = parser.parse_args()
     print(args)
     PROJ = args.proj
-    imgs = f"{PROJ}/images"
-    labels = f"{PROJ}/labels"
-    tag = []
-    for file in os.listdir(imgs):
-        if not file.endswith(".jpg"):
-            continue
-        prefix = file.rsplit(".", 1)[0]
-        if not os.path.exists(f"{labels}/{prefix}.txt"):
-            print(f"{labels}/{prefix}.txt miss")
-            continue
-        tag.append(prefix)
-    print(f"total images: {len(tag)}")
     if args.shuffle == 0:
         random.seed(42)
-    random.shuffle(tag)
-    print(f"shuffled, the first: {tag[0]}")
-    train_size = int(args.proportion * len(tag))
-    valid_size = len(tag) - train_size
-    print(f"train:{train_size} valid:{valid_size}")
-    move_data(PROJ,"train",tag[:train_size])
-    move_data(PROJ,"valid",tag[train_size:])
+    imgs = f"{PROJ}/images"
+    if args.mode == "cls":
+        path = Path(imgs)  # images dir
+        files = sorted(x for x in path.rglob('*.*') if x.suffix[1:].lower() in IMG_FORMATS)  # image files only
+        print(f"total images: {len(files)}")
+        random.shuffle(files)
+        print(f"shuffled, the first: {files[0]}")
+        train_size = int(args.proportion * len(files))
+        valid_size = len(files) - train_size
+        move_data_cls(PROJ,"train",files[:train_size])
+        move_data_cls(PROJ,"val",files[train_size:])
+    
+    elif args.mode == "seg":
+        labels = f"{PROJ}/labels"
+        tag = []
+        for file in os.listdir(imgs):
+            if not file.endswith(".jpg"):
+                continue
+            prefix = file.rsplit(".", 1)[0]
+            if not os.path.exists(f"{labels}/{prefix}.txt"):
+                print(f"{labels}/{prefix}.txt miss")
+                continue
+            tag.append(prefix)
+        print(f"total images: {len(tag)}")
+        random.shuffle(tag)
+        print(f"shuffled, the first: {tag[0]}")
+        train_size = int(args.proportion * len(tag))
+        valid_size = len(tag) - train_size
+        print(f"train:{train_size} valid:{valid_size}")
+        move_data(PROJ,"train",tag[:train_size])
+        move_data(PROJ,"valid",tag[train_size:])
 
 if __name__ == "__main__":
     main()
